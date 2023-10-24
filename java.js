528977f70922ca72ad330ba4b249b033165ebf5b
ThiagoAlvesRamos <141760665+ThiagoAlvesRamos@users.noreply.github.com>
2023-10-24 17:10:08 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,44 @@
+// function calcular() {
+//     let num = Number(window.prompt('Digite um numero:'))
+
+//     let res = document.querySelector('section#result')
+//     res.innerHTML = `<p> o numero a ser analisado aqui sera o <strong> ${num} </strong> </p>`
+
+//     res.innerHTML += `<p> o seu numero absoluto é <strong> ${Math.abs(num)} </strong> </p>`
+
+//     res.innerHTML += `<p> a sua parte inteira é <strong> ${Math.trunc(num)} </strong> </p>`
+
+//     res.innerHTML += `<p> o valor inteiro mais proximo é <strong> ${Math.round(num)} </strong> </p>`
+
+//     res.innerHTML += `<p> a sua raiz quadrada é <strong> ${Math.sqrt(num)} </strong> </p>`
+
+//     res.innerHTML += `<p> a sua raiz cubica é <strong> ${Math.cbrt(num)} </strong> </p>`
+
+//     res.innerHTML += `<p> o valor de ${num} <sup>2</sup> é ${Math.pow(num, 2)} </strong> </p>`
+
+//     res.innerHTML += `<p> o valor de ${num} <sup>3</sup> é ${Math.pow(num, 3)} </strong> </p>`
+// }
+// let contador = 0
+// let res = document.querySelector(`section#result`)
+// function contar() {
+//     contador++
+//     res.innerHTML = `<p>o contador está em <strong>${contador}</strong></p>`
+
+// }
+
+// function zerar() {
+//     contador = 0
+//     res.innerHTML = null
+
+// }
+function calculo() {
+    let a = number(document.getElementByild('a').value);
+    let b = number(document.getElementByild('b').value);
+    let c = number(document.getElementByild('c').value);
+    document.geSelector(`#result`).innerHTML = `a soma de <mark>${a}+ ${b}</mark> dividido por <strong> é igual a  <strong> ${(a + b) / c}</strong>
+
+}
+
+
+
+
